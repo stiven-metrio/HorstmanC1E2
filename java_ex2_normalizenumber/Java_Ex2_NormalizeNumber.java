a01d3d7bfeee97de346cb9d3fa2446058fb1e8e4
Jhonatan <jhonatanmetrio@gmail.com>
2019-01-20 23:21:03 -0500

    Calculate positive angles

---
+++ java_ex2_normalizenumber/Java_Ex2_NormalizeNumber.java
@@ -0,0 +1,41 @@
+/**
+ * Cay S. Horstmann -- Chapter 1
+ * Exercise #2
+ * Write a program that reads an integer angle 
+ * (which may be positive or negative) and normalizes it to a value 
+ * between 0 and 359 degrees. Try it first with the % operator, 
+ * then with floorMod.
+ */
+package java_ex2_normalizenumber;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jhonatan
+ */
+public class Java_Ex2_NormalizeNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Read an integer value
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter an integer value: ");
+        int value = scan.nextInt();
+        
+        // Normalize angle using % operator
+        int remainder = value % 360;
+        int quotient = value / 360;
+        System.out.println(remainder);
+        System.out.println(quotient);
+        
+        int angle = 0;
+        if(value >= 0)
+            angle = value-quotient*360;
+        if(value < 0);
+        System.out.printf("Angle: %d%n",angle);
+    }
+    
+}
